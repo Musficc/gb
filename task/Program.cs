b11f236bcbf275fe04d16087a33595158cb73cd3
Musficc <t81218942@gmail.com>
2023-06-20 10:20:37 +0300

    task complete

--- task/Program.cs
+++ task/Program.cs
@@ -1,4 +1,4 @@
-﻿string[] stringArray = new string[] { "Pr0", "1230", "J'aime le lasan", "Зонтик", "d@m", ",..,", "ga", "123456987", "raj" };
+﻿string[] stringArray = new string[] { "Pr0", "1230", "J'aime le lasan", "Зонтик", "d@m", ",..,", "ga", "123456987", "raj", ":)" };
 string[] resultArray = new string[0];
 
 void outputArray(string[] inputArray)
@@ -10,14 +10,29 @@ void outputArray(string[] inputArray)
     System.Console.WriteLine("\n");
 }
 
-void findStringThreeChars(string[] inputArray)
+string[] findStringThreeChars(string[] inputArray)
 {
     string[] resultArray = new string[0];
+    int j;
     for (int i = 0; i < inputArray.Length; i++)
     {
-        
+        j = 0;
+        while (j < inputArray[i].Length)
+        {
+            j++;
+        }
+        if (j <= 3)
+        {
+            Array.Resize(ref resultArray, resultArray.Length + 1);
+            resultArray[resultArray.Length - 1] = inputArray[i];
+        }
     }
+    return resultArray;
 }
 
-System.Console.WriteLine("\nЗадан массив:");
+System.Console.WriteLine("\nЗадан массив строк:");
 outputArray(stringArray);
+
+resultArray = findStringThreeChars(stringArray);
+System.Console.WriteLine("Его элементы, у которых три и меньше символов:");
+outputArray(resultArray);
