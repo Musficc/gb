09496e8238e6a1727907f3fd626f55a1fedecff0
Musficc <t81218942@gmail.com>
2023-06-20 10:05:41 +0300

    outputArray method added

--- task/Program.cs
+++ task/Program.cs
@@ -1,2 +1,23 @@
-﻿string[] stringArray = new string[]{"Abcd", "1230", "J'aime le lasan", "Зонтик", "@", ",..,"};
-string[] resultArray = new string[]{};
+﻿string[] stringArray = new string[] { "Pr0", "1230", "J'aime le lasan", "Зонтик", "d@m", ",..,", "ga", "123456987", "raj" };
+string[] resultArray = new string[0];
+
+void outputArray(string[] inputArray)
+{
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        System.Console.Write($"{inputArray[i]} | ");
+    }
+    System.Console.WriteLine("\n");
+}
+
+void findStringThreeChars(string[] inputArray)
+{
+    string[] resultArray = new string[0];
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        
+    }
+}
+
+System.Console.WriteLine("\nЗадан массив:");
+outputArray(stringArray);
